6cc56d033a144637f798096ccdce665a3c5e5892
HemanthGowda01 <tinycamvlogs@gmail.com>
2023-06-29 22:16:33 +0530

    code for docker build scan-modify

--- vars/dockerImageScan.groovy
+++ vars/dockerImageScan.groovy
@@ -1,7 +1,7 @@
 def call (String project, String ImageTag, String hubUser)
 {
     sh """
-    trivy image ${hubUser}/${project} ${hubUser}/${project}:latest > imagescanResult.txt
+    trivy image ${hubUser}/${project}:latest > imagescanResult.txt
     cat imagescanResult.txt
     """
 }

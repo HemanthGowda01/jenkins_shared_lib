5d42445e5120b3ec8aee224c6bfc377d652e0ec0
HemanthGowda01 <tinycamvlogs@gmail.com>
2023-06-30 00:09:44 +0530

    new code for ecr push and scan and other changes

--- vars/dockerImageScan.groovy
+++ vars/dockerImageScan.groovy
@@ -1,7 +1,19 @@
-def call (String project, String ImageTag, String hubUser)
+// def call (String project, String ImageTag, String hubUser)
+// {
+//     sh """
+//     trivy image ${hubUser}/${project}:latest > imagescanResult.txt
+//     cat imagescanResult.txt
+//     """
+// }
+
+
+def call (String aws_account_id, String region, String ecr_repoName)
 {
     sh """
-    trivy image ${hubUser}/${project}:latest > imagescanResult.txt
-    cat imagescanResult.txt
+
+    
+     trivy image  ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest > devDozeeBuild3.16.0result.txt
+     cat devDozeeBuild3.16.0result.txt
+
     """
 }

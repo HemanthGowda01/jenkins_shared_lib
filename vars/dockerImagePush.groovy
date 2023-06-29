3de32536409d8d14c4dd62bffdcf0b1b23f7ceb9
HemanthGowda01 <tinycamvlogs@gmail.com>
2023-06-30 03:03:10 +0530

    new modified code same old file - clean and build,scan other changes

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -12,13 +12,18 @@
 
 // above is the code for docker hub related
 
+import java.util.Random
 
 def call (String aws_account_id, String region, String ecr_repoName)
+
 {
+    def random = new Random()
+    def randomTag = "Build${random.nextInt(1000)}"
+
     sh """
 
     aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
-    docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
+    docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:${randomTag}
 
     """
 }
